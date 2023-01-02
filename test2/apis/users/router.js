52e8c095e856fcd7c82907f54ce3ee7d3501fa1e
haiqal <muhammadhaiqaldani@gmail.com>
2023-01-02 18:19:27 +0700

    add file

--- test2/apis/users/router.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const { viewSignin, actionSignin, actionLogout } = require('./controller')
-
-router.get('/', viewSignin);
-router.post('/', actionSignin);
-router.get('/logout', actionLogout);
-module.exports = router;
