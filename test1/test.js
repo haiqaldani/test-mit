c24f39b3a1531f0aa30e1389df8364e9b454b3f1
haiqal <muhammadhaiqaldani@gmail.com>
2023-01-02 18:08:12 +0700

    first commit

---
+++ test1/test.js
@@ -0,0 +1,10 @@
+function printTable() {
+  for (let i = 1; i <= 5; i++) {
+    let row = "";
+    for (let j = 1; j <= 5; j++) {
+      row += `${i * j} `;
+    }
+    console.log(row);
+  }
+}
+printTable()
